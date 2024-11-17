0dd585ecee5e3c9984bcd04adc701ddb94f216d7
MichaelAroca <Michaelaroca24@gmail.com>
2024-11-17 18:34:34 -0500

    PROYECTOWEBAVANZADOP1

--- partystore-app/src/app/app.routes.ts
+++ partystore-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { UsuariosComponent } from './components/crud-usuarios/crud-usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "crud-usuario",component:UsuariosComponent},
+
+
+    {path:"**", redirectTo:"crud-usuario" },
+
+
+];
