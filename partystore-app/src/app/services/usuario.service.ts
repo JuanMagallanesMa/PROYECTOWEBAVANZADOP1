397ebe26004962c9c8b7123144fc356bc5d6d338
MichaelAroca <Michaelaroca24@gmail.com>
2024-11-17 14:31:33 -0500

    PROYECTOWEBAVANZADOP1

--- partystore-app/src/app/services/usuario.service.ts
+++ partystore-app/src/app/services/usuario.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/Usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  private apiUrl = '../json/usuarios.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.apiUrl);
+  }
+
+  addUsuario(usuario: Usuario): Observable<Usuario> {
+    return new Observable((observer) => {
+      observer.next(usuario);
+      observer.complete();
+    });
+  }
+
+  updateUsuario(usuario: Usuario): Observable<Usuario> {
+    return new Observable((observer) => {
+      observer.next(usuario);
+      observer.complete();
+    });
+  }
+
+  deleteUsuario(id: number): Observable<void> {
+    return new Observable((observer) => {
+      observer.next();
+      observer.complete();
+    });
+  }
 }
+
