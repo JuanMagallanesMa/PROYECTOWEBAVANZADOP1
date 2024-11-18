6e7e058ad1f6faf715f5c4a25c3c3fbbdaf72ec9
JuanMagallanesMa <122581946+JuanMagallanesMa@users.noreply.github.com>
2024-11-17 19:07:09 -0500

    cambios

--- partystore-app/src/app/app.routes.ts
+++ partystore-app/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { CrudPedidosComponent } from './components/crud-pedidos/crud-pedidos.component';
+import { UsuariosComponent } from './components/crud-usuarios/crud-usuarios.component';
 
 export const routes: Routes = [
 
     {path:"pedidos", component: CrudPedidosComponent},
-    
+    {path: "crud-usuario",component:UsuariosComponent}
 
     //rutas por defecto
     {path: " ", redirectTo:"pedidos", pathMatch: 'full'},
