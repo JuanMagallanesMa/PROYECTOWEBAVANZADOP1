0f91e9f821289d766757233e96536f034106d5f6
MichaelAroca <Michaelaroca24@gmail.com>
2024-11-17 21:01:38 -0500

    PROYECTOWEBAVANZADOP1

--- partystore-app/src/app/app.routes.ts
+++ partystore-app/src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { UsuariosComponent } from './components/crud-usuarios/crud-usuarios.comp
 export const routes: Routes = [
 
     {path:"pedidos", component: CrudPedidosComponent},
-    {path: "usuario",component:UsuariosComponent},
+    {path:"usuario", component: UsuariosComponent},
 
     //rutas por defecto
     {path: " ", redirectTo:"usuario", pathMatch: 'full'},
-    {path:"**", redirectTo:"usuario"}, //** representa cualquier otra ruta */
+    {path:"**", redirectTo:"usuario"}, 
 ];
