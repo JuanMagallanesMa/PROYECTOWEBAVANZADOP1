c444c8c2173c626824a5b8ddb1d049ad4c5c92c5
JoffreArriag <153702003+JoffreArriag@users.noreply.github.com>
2024-11-18 20:57:04 -0500

    Update categoria.service.ts

--- partystore-app/src/app/services/categoria.service.ts
+++ partystore-app/src/app/services/categoria.service.ts
@@ -8,30 +8,27 @@ import { Categoria } from '../models/Categoria';
 })
 export class CategoriaService {
 
-  private apiUrl = 'assets/json/categorias.json';  // Ruta del archivo JSON local
+  private apiUrl = '../public/json/datos.json'; 
 
   constructor(private http: HttpClient) { }
 
-  // Obtener todas las categorías
+ 
   getCategorias(): Observable<Categoria[]> {
     return this.http.get<Categoria[]>(this.apiUrl);
   }
 
-  // Crear una nueva categoría (simulada)
+
   createCategoria(categoria: Categoria): Observable<Categoria> {
-    // Lógica para agregar la categoría al JSON (simulada)
     return this.http.post<Categoria>(this.apiUrl, categoria);
   }
 
-  // Actualizar una categoría (simulada)
+
   updateCategoria(id: number, categoria: Categoria): Observable<Categoria> {
-    // Lógica para actualizar la categoría en el JSON (simulada)
     return this.http.put<Categoria>(`${this.apiUrl}/${id}`, categoria);
   }
 
-  // Eliminar una categoría (simulada)
+
   deleteCategoria(id: number): Observable<void> {
-    // Lógica para eliminar la categoría del JSON (simulada)
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
