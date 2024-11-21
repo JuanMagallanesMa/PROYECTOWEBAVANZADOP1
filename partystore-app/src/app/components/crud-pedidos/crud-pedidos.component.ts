c097b6db7e88f538632496db461ab4be56355b21
JuanMagallanesMa <122581946+JuanMagallanesMa@users.noreply.github.com>
2024-11-21 17:30:07 -0500

    fin tabla reutili

--- partystore-app/src/app/components/crud-pedidos/crud-pedidos.component.ts
+++ partystore-app/src/app/components/crud-pedidos/crud-pedidos.component.ts
@@ -1,19 +1,17 @@
 
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { Usuario } from '../../models/Usuario';
-import { UsuarioService } from '../../services/usuario.service';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/Usuario';
 import { TableComponent } from '../shared/table/table.component';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { Producto, ProductoService } from '../../services/producto.service';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-crud-pedidos',
@@ -26,8 +24,8 @@ import { Producto, ProductoService } from '../../services/producto.service';
     MatAutocompleteModule,
     MatFormFieldModule,    
     MatInputModule, 
-    TableComponent,
-    MatPaginatorModule     
+  TableComponent  ,
+    MatPaginatorModule,     
   ]
 })
 export class CrudPedidosComponent implements OnInit {
@@ -41,35 +39,34 @@ export class CrudPedidosComponent implements OnInit {
   filteredOptions!: Observable<Usuario[]>;
   
   //datasource para la tabla
-  dataSource = new MatTableDataSource<Usuario>;
+  dataSource = new MatTableDataSource<Usuario>();
   
   //definir las columnas
- displayedColumns: string[] = ['nombreCompleto', 'correo', 'telefono'];
+  displayedColumns: string[] = ['nombreCompleto', 'correo', 'telefono', 'acciones']; // No incluir 'acciones' aquí 
+  columnAliases = { nombreCompleto: 'Nombre Completo', correo: 'Correo Electrónico', telefono: 'Teléfono', acciones:'Acciones' }; 
 
-  //constructor
-  constructor(private servicioUsuario : UsuarioService){
 
-  }
-  
+  constructor(private servicioUsuario : UsuarioService) { }
+
   ngOnInit() {
-    this.cargarTabla();
+    
     this.getUsuarios();
   }
-  cargarTabla():void{
-    this.servicioUsuario.getUsuarios().subscribe(data => { this.dataSource.data = data; });
-  }
 
-  getUsuarios():void{
-    this.servicioUsuario.getUsuarios().subscribe((data:Usuario[])=>{
-      this.options=data;
+  
+
+  getUsuarios(): void {
+    this.servicioUsuario.getUsuarios().subscribe((data: Usuario[]) => {
+      this.options = data;
+      this.dataSource.data = data; 
       this.filteredOptions = this.myControl.valueChanges.pipe(
         startWith(''),  // Cuando el input está vacío, se muestran todas las opciones
-        map(value => this._filter(value||''))  // Filtramos las opciones según el valor
+        map(value => this._filter(value || ''))  // Filtramos las opciones según el valor
       );
       console.log("Se muestra el autocomplete de usuarios");
     });
-    
   }
+
   // Función de filtro para las opciones
   private _filter(value: string): Usuario[] {
     const filterValue = value.toLowerCase();  // Comparamos sin importar mayúsculas/minúsculas
@@ -77,5 +74,15 @@ export class CrudPedidosComponent implements OnInit {
       option.nombreCompleto.toLowerCase().includes(filterValue) ||
       option.correo.toLowerCase().includes(filterValue)
     );
+  }
+
+  handleEdit(usuario: Usuario) { 
+    // Lógica para editar el usuario 
+    console.log('Editar usuario:', usuario); 
+  } 
+
+  handleDelete(usuario: Usuario) { 
+    // Lógica para eliminar el usuario 
+    console.log('Eliminar usuario:', usuario);
   }
 }
--- partystore-app/src/app/components/shared/table/table.component.ts
+++ partystore-app/src/app/components/shared/table/table.component.ts
@@ -1,12 +1,13 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core'; 
+import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core'; 
 import { MatTableDataSource, MatTableModule } from '@angular/material/table'; 
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator'; 
 import { MatSortModule } from '@angular/material/sort'
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule,MatSortModule,CommonModule],
+  imports: [MatTableModule, MatPaginatorModule,MatSortModule,CommonModule, MatButtonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
@@ -16,5 +17,9 @@ export class TableComponent implements AfterViewInit {
   }
   @Input() dataSource!: MatTableDataSource<any>; 
   @Input() displayedColumns!: string[];
+  @Input() columnAliases: { [key: string]: string } = {}; 
+  
+  @Output() onEdit = new EventEmitter<any>(); 
+  @Output() onDelete = new EventEmitter<any>();
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 }
