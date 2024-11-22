c9c4098053c14978051a5e9c944ce6ee0b83cfe6
JoffreArriag <153702003+JoffreArriag@users.noreply.github.com>
2024-11-22 13:07:40 -0500

    Update app.routes.ts

--- partystore-app/src/app/app.routes.ts
+++ partystore-app/src/app/app.routes.ts
@@ -2,16 +2,13 @@ import { Routes } from '@angular/router';
 import { CrudPedidosComponent } from './components/crud-pedidos/crud-pedidos.component';
 import { CrudUsuarioComponent } from './components/crud-usuario/crud-usuario.component';
 import { CrudProductoComponent } from './components/crud-producto/crud-producto.component';
-import { CrudCategoriaComponent } from './components/crud-categoria/crud-categoria.component';
-
 
 export const routes: Routes = [
 
     {path:"pedidos", component: CrudPedidosComponent},
     {path:"usuario", component: CrudUsuarioComponent},
     {path:"producto", component: CrudProductoComponent},
-    {path: "crud-categoria",component:CrudCategoriaComponent},
-
+    
     //rutas por defecto
     {path: " ", redirectTo:"pedidos", pathMatch: 'full'},
     {path:"**", redirectTo:"pedidos"}, 
@@ -19,4 +16,9 @@ export const routes: Routes = [
     {path: " ", redirectTo:"usuario", pathMatch: 'full'},
     {path:"**", redirectTo:"usuario"}, 
 
+    {path: " ", redirectTo:"prodcutos", pathMatch: 'full'},
+    {path:"**", redirectTo:"prodcutos"}, 
+
+    {path: " ", redirectTo:"prodcuto", pathMatch: 'full'},
+    {path:"**", redirectTo:"prodcuto"}, 
 ];
