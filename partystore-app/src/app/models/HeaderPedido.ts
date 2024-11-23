9e349cf1a74330773fe7a933e38a178c3041529f
JuanMagallanesMa <122581946+JuanMagallanesMa@users.noreply.github.com>
2024-11-23 15:34:30 -0500

    cmb

--- partystore-app/src/app/models/HeaderPedido.ts
+++ partystore-app/src/app/models/HeaderPedido.ts
@@ -1,6 +1,14 @@
 export interface HeaderPedido{
     idHeaderPedido ?: number;
     idUsuario: number;
+    nombresCompletos:string;
+    cedula: string;
+    telefono: string;
+    email: string;
+    provincia: string;
+    ciudad:string;
+    zip:string;
+    direccion:string;
     date: Date;
     SubTotal: number;
     IVA: 0.15;
