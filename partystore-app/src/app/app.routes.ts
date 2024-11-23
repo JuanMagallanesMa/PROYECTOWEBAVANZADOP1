af79864096fae9167b6be85f97b02dee5dcf0963
DAXENIL201 <126818058+DAXENIL201@users.noreply.github.com>
2024-11-22 22:50:26 -0500

    qq

--- partystore-app/src/app/app.routes.ts
+++ partystore-app/src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { CrudPedidosComponent } from './components/crud-pedidos/crud-pedidos.component';
 import { CrudUsuarioComponent } from './components/crud-usuario/crud-usuario.component';
 import { CrudProductoComponent } from './components/crud-producto/crud-producto.component';
+import { CrudCategoriaComponent } from './components/crud-categoria/crud-categoria.component';
 
 export const routes: Routes = [
 
     {path:"pedidos", component: CrudPedidosComponent},
     {path:"usuario", component: CrudUsuarioComponent},
     {path:"producto", component: CrudProductoComponent},
-    
+    {path: "crud-categoria",component:CrudCategoriaComponent},
+
     //rutas por defecto
     {path: " ", redirectTo:"pedidos", pathMatch: 'full'},
     {path:"**", redirectTo:"pedidos"}, 
