ddaeed8bd3f7cfc1c5e00da063dd390d7854caf9
JuanMagallanesMa <122581946+JuanMagallanesMa@users.noreply.github.com>
2025-01-19 11:44:05 -0500

    sd

--- partystore-app/src/app/app.config.ts
+++ partystore-app/src/app/app.config.ts
@@ -9,6 +9,6 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
-    provideHttpClient(), provideAnimationsAsync()
+    provideHttpClient(),
   ]
 };
