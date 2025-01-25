5dbedec65e8e18fb96b0d57e93bf9da209717294
Erickabs <ericka.bonetes@ug.edu.ec>
2025-01-25 16:13:35 -0500

    modi css

--- partystore-app/src/app/components/crud-promociones/crud-promociones.component.ts
+++ partystore-app/src/app/components/crud-promociones/crud-promociones.component.ts
@@ -10,40 +10,12 @@ import { Promociones } from '../../models/Promociones';
   templateUrl: './crud-promociones.component.html',
   styleUrls: ['./crud-promociones.component.css']
 })
-export class CrudPromocionesComponent {
+export class CrudPromocionesComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   promociones: Promociones[] = [
-    CrudPromocionesComponent.constructor(PromocionesService , PromocionesService)
-    
-    ,
-    {
-      id: 1,
-      nombre: 'Descuento 20% en toda la tienda',
-      descripcion: 'Disfruta de un 20% de descuento en todos tus productos favoritos.',
-      descuentoPorcentaje: 20,
-      fechaFin: new Date('2025-01-25'),
-      isActive: true,
-    }
-    ,
-    {
-      id: 2,
-      nombre: 'Compra 2, Paga 1',
-      descripcion: 'Llévate el segundo producto igual al primero completamente gratis.',
-      fechaFin: new Date('2025-02-06'),
-      isActive: true,
-      descuentoPorcentaje: 50
-    }
-    ,
-    {
-      id: 3,
-      nombre: 'Envío gratis en compras superiores a $30',
-      descripcion: 'Disfruta de envío gratis en todas tus compras superiores a $30.',
-      fechaFin: new Date('2025-01-30'),
-      isActive: true,
-      descuentoPorcentaje: 20
-    }
-  ];
+    ];
 }
 
-function ngOnInit(): Promociones {
-  throw new Error('Function not implemented.');
-}
+
